b3f8d7d702beb862ac0e29fe8f5cf4baa02824e1
arezooh <48172387+arezooh@users.noreply.github.com>
2021-04-11 04:55:56 +0430

    adding code to change the target mode to normal

---
+++ get_normal_target.py
@@ -0,0 +1,117 @@
+def get_normal_target(training_target, test_target, training_prediction, test_prediction, target_mode, target_granularity = None):
+    
+    '''
+    training_target : a data frame including columns 'spatial id', 'temporal id', 'Target', 'normal target'
+                        from the training set
+                        
+    test_target : a data frame including columns 'spatial id', 'temporal id', 'Target', 'normal target'
+                        from the test set
+                        
+    training_prediction : list of predicted values for the training set
+    
+    test_prediction : list of predicted values for the test set
+    
+    target_mode = 'normal' , 'cumulative' , 'moving average', 'differential' the mod of the target variable
+    
+    target_granularity : number of smaller temporal units which is averaged to get the moving average target
+    
+    '''
+    
+    if target_mode == 'normal':
+        return training_target, test_target, training_prediction, test_prediction
+    
+    training_target.loc[:,('type')] = 1
+    test_target.loc[:,('type')] = 2
+    
+    # if target mode is cumulative we need to return the target variable to its original state
+    if target_mode == 'cumulative':
+        
+        data = training_target.append(test_target)
+        
+        # practical accessible values (target real values in training set and predicted values in test set) will be used 
+        # for returning the cumulative target (predicted) to original state
+
+        data.loc[:,('train_real_test_prediction')] = list(training_target['Target']) + list(test_prediction)
+        data.loc[:,('prediction')] = list(training_prediction) + list(test_prediction)
+        data = data.sort_values(by=['temporal id', 'spatial id'])
+        reverse_dates = data['temporal id'].unique()[::-1]
+        
+        for index in range(len(reverse_dates)):
+            date = reverse_dates[index]
+            past_date = reverse_dates[index + 1]
+            data.loc[data['temporal id'] == date, 'prediction'] = list(
+                np.array(data.loc[data['temporal id'] == date, 'prediction']) - np.array(
+                    data.loc[data['temporal id'] == past_date, 'train_real_test_prediction']))
+            
+            if index == len(reverse_dates) - 2:
+                break
+
+    # if target mode is moving average we need to return the target variable to its original state
+    if target_mode == 'moving average':
+
+        data = training_target.append(test_target)
+        
+        # practical accessible values (target real values in training set and predicted values in test set) will be used 
+        # for returning the moving average target (predicted) to original state
+        data.loc[:,('train_real_test_prediction')] = list(training_target['normal target']) + list(test_prediction)
+        data.loc[:,('prediction')] = list(training_prediction) + list(test_prediction)
+
+        data = data.sort_values(by=['temporal id', 'spatial id'])
+
+        target_granularity = int(target_granularity)
+        
+        dates = data['temporal id'].unique()
+        for index in range(len(dates)):
+            ind = index + target_granularity - 1
+            date = dates[ind]
+            data.loc[data['temporal id'] == date, 'prediction'] = list(target_granularity * np.array(
+                data.loc[data['temporal id'] == date, 'prediction']))
+            
+            for i in range(target_granularity - 1):
+                past_date = dates[ind - (i + 1)]
+                data.loc[
+                    data['temporal id'] == date, 'prediction'] = list(np.array(
+                    data.loc[data['temporal id'] == date, 'prediction']) - np.array(
+                    data.loc[data['temporal id'] == past_date, 'train_real_test_prediction']))
+            if ind == len(dates) - 1:
+                break
+
+    # if target mode is differential we need to return the target variable to its original state
+    if target_mode == 'differential':
+
+        data = training_target.append(test_target)
+        
+        # practical accessible values (target real values in training set and predicted values in test set) will be used 
+        # for returning the differential target (predicted) to original state
+        data.loc[:,('train_real_test_prediction')] = list(training_target['normal target']) + list(test_prediction)
+        data.loc[:,('prediction')] = list(training_prediction) + list(test_prediction)
+
+        data = data.sort_values(by=['temporal id', 'spatial id'])
+        
+        dates = data['temporal id'].unique()
+        for index in range(len(dates)):
+            date = dates[index + 1]
+            past_date = dates[index]
+            data.loc[
+                data['temporal id'] == date, 'prediction'] = list(np.array(
+                data.loc[data['temporal id'] == date, 'prediction']) + np.array(
+                data.loc[data['temporal id'] == past_date, 'train_real_test_prediction']))
+            
+            if index == len(dates) - 2:
+                break
+                
+                
+    data = data.sort_values(by=['temporal id', 'spatial id'])
+    training_set = data[data['type'] == 1]
+    test_set = data[data['type'] == 2]
+    # cause of warning:
+    training_set.loc[:,('Target')] = training_set['normal target']
+    test_set.loc[:,('Target')] = test_set['normal target']
+
+    training_prediction = list(training_set['prediction'])
+    test_prediction = list(test_set['prediction'])
+
+    training_target = training_set.drop(['type','prediction','train_real_test_prediction'], axis = 1)
+    test_target = test_set.drop(['type','prediction','train_real_test_prediction'], axis = 1)
+    
+    return training_target, test_target, training_prediction, test_prediction
