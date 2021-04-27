09bc718be6de3944836dfa904680bd3897aa48e4
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2021-04-27 11:43:26 +0430

    update prediction

--- configurations.py
+++ configurations.py
@@ -18,3 +18,21 @@ TEST_TYPES = ['one-by-one', 'whole-as-one']
 TARGET_MODES = ['normal', 'cumulative', 'differential', 'moving_average']
 
 RANKING_METHODS = ['mRMR', 'CORRELATION']
+
+FEATURE_SELECTION_TYPES = ['covariate', 'feature']
+
+PRE_DEFINED_MODELS = ['nn', 'knn', 'glm', 'gbm']
+
+MODEL_TYPES = ['regression', 'classification']
+
+PERFORMANCE_MEASURES = ['MAE', 'MAPE', 'MASE', 'MSE', 'R2_score']
+
+PERFORMANCE_BENCHMARKS = ['MAPE']
+
+FEATURE_SCALERS = ['logarithmic', 'normalize', 'standardize', None]
+
+TARGET_SCALERS = ['logarithmic', 'normalize', 'standardize', None]
+
+SPLITTING_TYPES = ['training-validation', 'cross-validation']
+
+VERBOSE_OPTIONS = [0, 1, 2]
--- predict.py
+++ predict.py
@@ -5,23 +5,132 @@ import pandas as pd
 import configurations
 import rank_covariates
 import rank_features
+import train_validate
 
 
 def predict(data: list,
-            test_type: str,
-            max_history_length: int,
-            forecast_horizon: int,
-            ranking_method: str,
-            forced_covariates: list):
+            forecast_horizon: int = 1,
+            feature_scaler: str = None,
+            target_scaler: str = None,
+            test_type: str = 'whole-as-one',
+            feature_sets: dict = {'covariate': 'mRMR'},
+            model_type: str = 'regression',
+            models: list = ['knn'],
+            instance_testing_size: int or float = 0.2,
+            splitting_type: str = 'training-validation',
+            instance_validation_size: int or float = 0.3,
+            instance_random_partitioning: bool = False,
+            fold_total_number: int = 5,
+            performance_benchmark: str = 'MAPE',
+            performance_measure: str = ['MAPE'],
+            performance_report: bool = True,
+            save_predictions: bool = True,
+            verbose: int = 0):
+    """
+
+    Args:
+        data:
+        forecast_horizon:
+        feature_scaler:
+        target_scaler:
+        test_type:
+        feature_sets:
+        model_type:
+        models:
+        instance_testing_size:
+        splitting_type:
+        instance_validation_size:
+        instance_random_partitioning:
+        fold_total_number:
+        performance_benchmark:
+        performance_measure:
+        performance_repost:
+        save_predictions:
+        verbose:
+
+    Returns:
+
+    """
     # input checking
+    # data input checking
     if not isinstance(data, list):
         sys.exit("The input data must be a list of DataFrames or a list of strings of data addresses.")
     str_check = [isinstance(d, str) for d in data]
     df_check = [isinstance(d, pd.DataFrame) for d in data]
     if not (all(str_check) or all(df_check)):
         sys.exit("The input data must be a list of DataFrames or a list of strings of data addresses.")
+    # forecast_horizon input checking
+    if not (isinstance(forecast_horizon, int) and forecast_horizon >= 1):
+        sys.exit("forecast_horizon is not valid.")
+    # feature_scaler input checking
+    if feature_scaler not in configurations.FEATURE_SCALERS:
+        sys.exit("feature_scaler input is not valid.")
+    # target_scaler input checking
+    if target_scaler not in configurations.TARGET_SCALERS:
+        sys.exit("target_scaler input is not valid.")  # test_type input checking
     if test_type not in configurations.TEST_TYPES:
-        sys.exit("Test type is not valid.")
+        sys.exit("test_type is not valid.")
+    # feature_sets input checking
+    if not (isinstance(feature_sets, dict) and len(feature_sets.keys()) == 1):
+        sys.exit("feature_sets input format is not valid.")
+    if not (list(feature_sets.keys())[0] in configurations.FEATURE_SELECTION_TYPES
+            and list(feature_sets.values())[0] in configurations.RANKING_METHODS):
+        sys.exit("feature_sets input is not valid.")
+    # model_type input checking
+    if model_type not in configurations.MODEL_TYPES:
+        sys.exit("model_type input is not valid.")
+    # models input checking
+    if not isinstance(models, list):
+        sys.exit("models input format is not valid.")
+    for model in models:
+        if isinstance(model, str):
+            if model not in configurations.PRE_DEFINED_MODELS:
+                sys.exit("models input is not valid.")
+        elif isinstance(model, dict):
+            if len(list(model.keys())) == 1:
+                if list(model.keys())[0] not in configurations.PRE_DEFINED_MODELS:
+                    sys.exit("models input is not valid.")
+            else:
+                sys.exit("models input is not valid.")
+        elif callable(model):
+            pass
+        else:
+            sys.exit("Models input is not valid.")
+    # instance_testing_size input checking
+    if not ((isinstance(instance_testing_size, float) and 0 < instance_testing_size < 1) or (
+            isinstance(instance_testing_size, int) and instance_testing_size > 0)):
+        sys.exit("instance_testing_size input is not valid.")
+    # splitting_type input checking
+    if splitting_type not in configurations.SPLITTING_TYPES:
+        sys.exit("splitting_type input is not valid.")
+    # instance_validation_size input checking
+    if not ((isinstance(instance_validation_size, float) and 0 < instance_validation_size < 1) or (
+            isinstance(instance_validation_size, int) and instance_validation_size > 0)):
+        sys.exit("instance_validation_size input is not valid.")
+    # instance_random_partitioning input checking
+    if not isinstance(instance_random_partitioning, bool):
+        sys.exit("instance_random_partitioning input is not valid.")
+    # fold_total_number input checking
+    if not (isinstance(fold_total_number, int), fold_total_number > 1):
+        sys.exit("fold_total_number input is not valid.")
+    # performance_benchmark input checking
+    if performance_benchmark not in configurations.PERFORMANCE_BENCHMARKS:
+        sys.exit("performance_benchmark input is not valid.")
+    # performance_measure input checking
+    if not isinstance(performance_measure, list):
+        sys.exit("performance_benchmark input format is not valid.")
+    for p_m in performance_measure:
+        if p_m not in configurations.PERFORMANCE_MEASURES:
+            sys.exit("performance_benchmark input is not valid.")
+    # performance_report input checking
+    if not isinstance(performance_report, bool):
+        sys.exit("performance_report input is not valid.")
+    # save_predictions input checking
+    if not isinstance(save_predictions, bool):
+        sys.exit("save_predictions input is not valid.")
+    # verbose input checking
+    if verbose not in configurations.VERBOSE_OPTIONS:
+        sys.exit("verbose input is not valid.")
 
     # data preparing
     if isinstance(data[0], str):
@@ -31,7 +140,7 @@ def predict(data: list,
             sys.exit(str(e))
 
     # data columns names manipulation
-    target_mode, target_granularity, granularity = None, None, None
+    target_mode, target_granularity, granularity = None, 1, 1
     target_column_name = list(filter(lambda x: x.startswith('Target '), data[0].columns.values))[0]
     temp = target_column_name.split(' ')[-1][1:-1]
     if temp.startswith('augmented'):
@@ -51,14 +160,47 @@ def predict(data: list,
     data = [d.rename(columns={target_column_name: target_column_name.split(' ')[0]}) for d in data]
 
     # ranking
-    ordered_covariates_or_features = rank_covariates.rank_covariates(data=pd.DataFrame.copy(data[2]),
-                                                                     ranking_method='mRMR')
+    feature_selection_type = list(feature_sets.keys())[0]
+    ranking_method = list(feature_sets.values())[0]
+    ordered_covariates_or_features = []
+    if feature_selection_type == 'covariate':
+        ordered_covariates_or_features = rank_covariates.rank_covariates(data=pd.DataFrame.copy(data[0]),
+                                                                         ranking_method=ranking_method)
+    else:
+        for d in data:
+            ordered_covariates_or_features.append(rank_features.rank_features(data=d,
+                                                                              ranking_method=ranking_method))
 
-    # train_validate
+    if test_type == 'whole-as-one':
+        # train_validate
+        best_model, best_model_parameters, best_history_length, best_feature_or_covariate_set, best_trained_model = \
+            train_validate.train_validate(data=data,
+                                          forecast_horizon=forecast_horizon,
+                                          input_scaler=feature_scaler,
+                                          output_scaler=target_scaler,
+                                          target_mode=target_mode,
+                                          target_granularity=target_granularity,
+                                          granularity=granularity,
+                                          ordered_covariates_or_features=ordered_covariates_or_features,
+                                          model_type=model_type,
+                                          models=models,
+                                          instance_testing_size=instance_testing_size,
+                                          splitting_type=splitting_type,
+                                          instance_validation_size=instance_validation_size,
+                                          instance_random_partitioning=instance_random_partitioning,
+                                          fold_total_number=fold_total_number,
+                                          performance_benchmark=performance_benchmark,
+                                          performance_measure=performance_measure,
+                                          performance_report=performance_report,
+                                          save_predictions=save_predictions,
+                                          verbose=verbose)
 
-    # train_test
+        # train_test
 
-    # predict_future
+        # predict_future
+
+    else:
+        pass
 
     return None
 
@@ -66,7 +208,5 @@ def predict(data: list,
 if __name__ == '__main__':
     predict(data=['historical_data h=1.csv', 'historical_data h=2.csv', 'historical_data h=3.csv'],
             test_type='whole-as-one',
-            max_history_length=3,
             forecast_horizon=5,
-            ranking_method='mRMR',
-            forced_covariates=[])
+            feature_sets={'covariate': 'mRMR'})
