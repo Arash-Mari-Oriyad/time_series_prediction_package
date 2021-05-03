6a0dcd15ef53c789bebb02bc64ab4f856713e81e
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2021-05-03 12:07:49 +0430

    fix get_future_data function problems

--- get_future_data.py
+++ get_future_data.py
@@ -4,15 +4,16 @@ import pandas as pd
 from get_target_quantities import get_target_quantities
 
 
-def get_future_data(data: pd.DataFrame,
+def get_future_data(data: list,
                     forecast_horizon: int):
-    if not isinstance(data, pd.DataFrame):
+
+    if not (isinstance(data, list) and all(isinstance(d, pd.DataFrame) for d in data)):
         sys.exit('data input format is not valid')
 
     if not (isinstance(forecast_horizon, int) and forecast_horizon >= 1):
         sys.exit('data input format is not valid')
 
-    _, _, granularity, _ = get_target_quantities(data)
+    _, _, granularity, _ = get_target_quantities(data[0])
 
     future_data = [d.iloc[-(forecast_horizon * granularity * len(d['spatial id'].unique())):].copy() for d in data]
     data = [d.iloc[:-(forecast_horizon * granularity * len(d['spatial id'].unique()))].copy() for d in data]
