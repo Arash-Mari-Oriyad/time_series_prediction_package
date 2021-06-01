f417b80873c40dd7acc144b456580a6af881c56b
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2021-06-01 15:18:34 +0430

    update mbp_data

--- mbp_data/manipulate_data.py
+++ mbp_data/manipulate_data.py
@@ -3,13 +3,15 @@ import pandas as pd
 mbp_data = pd.read_csv('main_historical_data h=1.csv')
 
 print(mbp_data.shape)
+print(mbp_data['Target (normal)'].unique())
 print(mbp_data['spatial id'].nunique())
 
-temp_spatial_ids = mbp_data['spatial id'].values.tolist()[:10]
+temp_spatial_ids = mbp_data['spatial id'].values.tolist()[:1000]
 
 changed_mbp_data = mbp_data[mbp_data['spatial id'].isin(temp_spatial_ids)].copy()
 
 print(changed_mbp_data.shape)
+print(changed_mbp_data['Target (normal)'].unique())
 print(changed_mbp_data['spatial id'].nunique())
 
 changed_mbp_data.to_csv('historical_data h=1.csv', index=False)
